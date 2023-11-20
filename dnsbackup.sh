cf376a97e893f6951faa929f2b6d590e0d58d419
IntenseWebs <iw@intensewebs.com>
2023-11-20 17:23:01 +0000

    Reinitialize Repository

---
+++ dnsbackup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd ~/bind-backups
+sudo cp -f /etc/bind/named.* .
+sudo cp -f /var/lib/bind/* .
+sudo cp -f /etc/resolv.conf .
+sudo cp -f /etc/hosts .
+sudo cp -f /etc/hostname .
+sudo cp -f /etc/network/interfaces .
+# scp -f * privacy@sd:/home/privacy/Repos/dns-backups/
